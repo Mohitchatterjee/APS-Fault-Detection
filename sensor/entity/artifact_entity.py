4eb3c502a6a1c9abc490d6f52cd817f63ad18421
Mohitchatterjee <chatterjeemohit160@gmail.com>
2022-12-10 15:14:01 +0530

    Model Trainer Commit

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -18,7 +18,11 @@ class DataTransformationArtifact:
     transformed_test_path:str
     target_encoder_path:str
 
-    
-class ModelTrainerArtifact:...
+@dataclass    
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
+
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -59,6 +59,13 @@ class DataTransformationConfig:
         self.target_encoder_path = os.path.join(self.data_transformation_dir,"target_encoder",TARGET_ENCODER_OBJECT_FILE_NAME)
 
         
-class ModelTrainerConfig:...
+class ModelTrainerConfig:
+    
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir = os.path.join(training_pipeline_config.artifact_dir , "model_trainer")
+        self.model_path = os.path.join(self.model_trainer_dir,"model",MODEL_FILE_NAME)
+        self.expected_score = 0.7
+        self.overfitting_threshold = 0.1
+
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
