c0e039fc9067ee3993321512a017132fe8974f7e
Mohitchatterjee <chatterjeemohit160@gmail.com>
2022-12-12 18:42:35 +0530

    Project Complete

--- main.py
+++ main.py
@@ -1,46 +1,12 @@
-from sensor.logger import logging
-from sensor.exception import SensorException
-from sensor.utils import get_collection_as_dataframe
-import sys,os
-from sensor.entity import config_entity
-from sensor.components.DataIngestion import DataIngestion
-from sensor.components.DataValidation import DataValidation
-from sensor.components.DataTransformation import DataTransformation
-from sensor.components.ModelTrainer import ModelTrainer
+from sensor.pipeline.training_pipeline import start_training_pipeline
+from sensor.pipeline.batch_prediction import start_batch_prediction
 
-
+file_path="/config/workspace/aps_failure_training_set1.csv"
+print(__name__)
 if __name__=="__main__":
      try:
-          training_pipeline_config = config_entity.TrainingPipelineConfig()
-
-          #data ingestion
-          data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-          print(data_ingestion_config.to_dict())
-          
-          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-          
-          #data validation
-          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
-          
-          data_validation = DataValidation(data_validation_config=data_validation_config,
-                         data_ingestion_artifact=data_ingestion_artifact)
-
-          data_validation_artifact = data_validation.initiate_data_validation()
-
-          # Data Transformation
- 
-          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
-          data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
-          data_ingestion_artifact=data_ingestion_artifact)
-          data_transformation_artifact = data_transformation.initiate_data_transformation()
-
-
-          #Model Trainer
-
-          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
-          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
-          model_trainer_artifact = model_trainer.initiate_model_trainer()
-          
+          # start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
      except Exception as e:
           print(e)
